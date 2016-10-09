7f4d6a4af6ff29aeee1719988a924a7cbe8ec432
bobanko <mishko.vladimir@gmail.com>
2016-10-10 02:34:21 +0300

    angular app added test component added  - still buggy

--- App/app.js
+++ App/app.js
@@ -1,1 +1,12 @@
-console.log('app.js init');
+'use strict';
+
+angular.module('powerGrid', [])
+    .run(function () {
+        //init stuff goes here
+        console.log('app.js init from ng');
+    })
+    .config([function ($httpProvider) {
+        $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+        $httpProvider.defaults.useXDomain = true;
+        $httpProvider.interceptors.push('XSRFTokenInterceptor');
+    }]);
--- App/index.js
+++ App/index.js
@@ -4,5 +4,4 @@ var getApiVersion = function(){
   
 };
 
-
 getApiVersion();
