9018783b7e2c48b0f85201570bdf917f93b291ac
bobanko <mishko.vladimir@gmail.com>
2016-10-12 01:00:06 +0300

    shitty CORS finally fucked himself

--- App/app.js
+++ App/app.js
@@ -2,11 +2,18 @@
 
 angular.module('powerGrid', [])
     .run(function () {
-    	//init stuff goes here
-    	console.log('app.js init from ng');
+        //init stuff goes here
+        console.log('app.js init from ng');
     })
     .config([function ($httpProvider) {
-     //   $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
-    //    $httpProvider.defaults.useXDomain = true;
-    //    $httpProvider.interceptors.push('XSRFTokenInterceptor');
+        //$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+        //    $httpProvider.defaults.useXDomain = true;
+        //    $httpProvider.interceptors.push('XSRFTokenInterceptor');
     }]);
+
+
+
+angular.module('powerGrid').config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+}]);
--- App/misc/version/version.js
+++ App/misc/version/version.js
@@ -1,20 +1,44 @@
 'use strict';
 
 var componentController = function ($http) {
-	var ctrl = this;
+    var ctrl = this;
 
 
     //todo: move to manager/service
 
-    var apiVerUrl= 'http://powergrid-api.azurewebsites.net/api/Version';
+     var apiVerUrl= 'http://powergrid-api.azurewebsites.net/api/version';
+
+    // var apiVerUrl = 'http://localhost:5000/api/version';
     //var apiVerUrl= 'http://powergrid-api.azurewebsites.net/api';
 
-    $http.get(apiVerUrl).then(function(data){
-        ctrl.value = data;
-        console.info('server responds ok',data);
-    },function(data){
+
+    // $http({
+    //     method: 'GET',
+    //     url: apiVerUrl,
+    //     headers: { Accept: 'application/json' }
+    // }).then(function (data) {
+    //     console.info(data);
+    // }, function (data) {
+    //     console.warn(data);
+    // });
+
+
+    // var jqxhr = $.ajax(apiVerUrl)
+    //     .done(function (data) {
+    //         console.log("success", data);
+    //     })
+    //     .fail(function () {
+    //         console.log("error");
+    //     });
+
+
+
+    $http.get(apiVerUrl).then(function (data) {
+        ctrl.value = data.data.data;
+        console.info('server responds ok', data);
+    }, function (data) {
         ctrl.value = 'FAILED TO LOAD VERSION';
-        console.warn('server responds bad',data);
+        console.warn('server responds bad', data);
     });
 
 
@@ -23,8 +47,8 @@ var componentController = function ($http) {
 
 angular.module('powerGrid')
     .component('version', {
-    	bindings: {
-    	},
-    	templateUrl: 'app/misc/version/version.html',
-    	controller: componentController
+        bindings: {
+        },
+        templateUrl: 'app/misc/version/version.html',
+        controller: componentController
     });
