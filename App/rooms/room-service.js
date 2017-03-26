25dcfe10be50a8aeb8ceb25fabea588da1a63fbe
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-03-26 21:12:33 +0300

    toggleReady added for room-service

--- App/rooms/room-service.js
+++ App/rooms/room-service.js
@@ -1,50 +1,51 @@
 ﻿'use strict';
 
 angular.module('rooms', [])
-.service('roomService', function ($q, apiEndpoints, apiWsEndpoints, authService) {
-	const svc = this;
+	.service('roomService', function ($q, apiEndpoints, apiWsEndpoints, authService) {
+		const svc = this;
 
 
-	svc.getRoomList = apiEndpoints.getRoomList;
+		svc.getRoomList = apiEndpoints.getRoomList;
 
-	//todo: client-side API mock
-	svc.getRoom = function (roomId) {
-		//return apiEndpoints.getRoom(roomId); //todo: nesuwestvuet!!1
-		return svc.getRoomList().then(function (rooms) {
-			var currentRoom = rooms.find((r) =>r['Id'] === roomId);
-			if (currentRoom)
-				return currentRoom;
-			else
-				return $q.reject({ message: 'no room found', roomId: roomId });
-		});
-	};
+		//todo: client-side API mock
+		svc.getRoom = function (roomId) {
+			//return apiEndpoints.getRoom(roomId); //todo: nesuwestvuet!!1
+			return svc.getRoomList().then(function (rooms) {
+				var currentRoom = rooms.find((r) => r['Id'] === roomId);
+				if (currentRoom)
+					return currentRoom;
+				else
+					return $q.reject({message: 'no room found', roomId: roomId});
+			});
+		};
 
-	svc.createRoom = function (roomName) {
-		return apiEndpoints.createRoom({ name: roomName });
-	};
+		svc.createRoom = function (roomName) {
+			return apiEndpoints.createRoom({name: roomName});
+		};
 
-	svc.joinRoom = function (roomId) {
-		return apiEndpoints.joinRoom({ roomId: roomId });
-	};
+		svc.joinRoom = function (roomId) {
+			return apiEndpoints.joinRoom({roomId: roomId});
+		};
 
-	svc.leaveRoom = function () {
-		return apiEndpoints.leaveRoom();
-	};
+		svc.leaveRoom = function () {
+			return apiEndpoints.leaveRoom();
+		};
 
-	svc.kickUser = function (userId) {
-		return apiEndpoints.kickUser({ username: userId });
-	};
+		svc.kickUser = function (userId) {
+			return apiEndpoints.kickUser({username: userId});
+		};
+
+		svc.toggleReady = function (isReady) {
+			return apiEndpoints.toggleReadyRoom({state: isReady});
+		};
 
 
-	svc.startGameRoom =  apiEndpoints.startGameRoom;
+		svc.startGameRoom = apiEndpoints.startGameRoom;
 
 
-	
-	
+		svc.onRoomsUpdated = function () {
+			//todo: podpisatsa nado
+			//apiWsEndpoints
+		};
 
-	svc.onRoomsUpdated = function () {
-		//todo: podpisatsa nado
-		//apiWsEndpoints
-	};
-
-});
+	});
