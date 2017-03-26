e4c60ea288260c7a0c44130106f02e4cb008e13e
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-03-26 21:10:34 +0300

    room-user component separated

---
+++ App/rooms/room/room-user/room-user.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var componentController = function () {
+	const ctrl = this;
+
+};
+
+angular.module('rooms')
+	.component('roomUser', {
+		bindings: {
+			user: '<',
+
+			hideKick:'<',
+			enableReady:'<',
+
+			onKick: '&?',
+			onReady: '&?',
+		},
+		templateUrl: 'app/rooms/room/room-user/room-user.html',
+		controller: componentController
+	});
--- App/rooms/room/room.js
+++ App/rooms/room/room.js
@@ -1,61 +1,59 @@
 'use strict';
 
-var componentController = function ($controller, $location, roomService, apiEndpoints) {
-    var ctrl = this;
+var componentController = function ($controller, $location, roomService, authService, errorHandler) {
+	const ctrl = this;
 
-    $controller('authCheck', {});
+	$controller('authCheck', {});
 
-    ctrl.room = function () {
+	ctrl.currentUser = authService.currentPlayer;
 
-        //apiEndpoints.getRoom(ctrl.roomId);
+	ctrl.toggleReady = function (isReady) {
+		roomService.toggleReady(isReady).then(data => {
 
-    };
-
-    ctrl.testCheck = true;
-
-
-    ctrl.kickUser = function (userId) {
-        roomService.kickUser(userId)
-            .then(() => ctrl.initRoom(ctrl.roomId));
-    };
+		});
+	};
 
 
-    ctrl.leaveRoom = function () {
-        roomService.leaveRoom().then(() => $location.path('/rooms'));
-    };
+	ctrl.kickUser = function (userId) {
+		roomService.kickUser(userId)
+			.then(() => ctrl.initRoom(ctrl.roomId));
+	};
 
 
-    ctrl.startGameRoom = function () {
-        roomService.startGameRoom()
-            .then((data) => console.info(data))
-            .then(() => $location.path('/game/'))
-            .catch((error) => {
-                console.warn(error.message);
-            });
-    };
-
-    ctrl.initRoom = function (roomId) {
-        roomService.getRoom(roomId).then(function (room) {
-            ctrl.room = room;
-        }, function (error) {
-            console.warn(error);
-            $location.path('/rooms');
-        });
-
-    };
+	ctrl.leaveRoom = function () {
+		roomService.leaveRoom().then(() => $location.path('/rooms'));
+	};
 
 
-    ctrl.$onInit = function () {
-        ctrl.initRoom(ctrl.roomId);
-    }
+	ctrl.startGameRoom = function () {
+		roomService.startGameRoom()
+			.then((data) => console.info(data))
+			.then(() => $location.path('/game/'))
+			.catch(errorHandler);
+	};
+
+	ctrl.initRoom = function (roomId) {
+		roomService.getRoom(roomId).then(function (room) {
+			ctrl.room = room;
+		}, function (error) {
+			console.warn(error);
+			$location.path('/rooms');
+		});
+
+	};
+
+
+	ctrl.$onInit = function () {
+		ctrl.initRoom(ctrl.roomId);
+	}
 
 };
 
 angular.module('rooms')
-    .component('room', {
-        bindings: {
-            roomId: '<'
-        },
-        templateUrl: 'app/rooms/room/room.html',
-        controller: componentController
-    });
+	.component('room', {
+		bindings: {
+			roomId: '<'
+		},
+		templateUrl: 'app/rooms/room/room.html',
+		controller: componentController
+	});
