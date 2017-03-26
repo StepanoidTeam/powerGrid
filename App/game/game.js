29f6ac6517d0cc88bdb65e77876dacc9e8681048
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-03-27 02:38:06 +0300

    basic game svg map impl in progress

--- App/game/game.js
+++ App/game/game.js
@@ -1,17 +1,39 @@
 'use strict';
 
-var componentController = function ($location, authService) {
-    const ctrl = this;
+var componentController = function ($location, gameService) {
+	const ctrl = this;
 
 
-    ctrl.$onInit = function () {
-        //todo: start game?
-    };
+	ctrl.$onInit = function () {
+
+		/*gameService.getMaps()
+			.then(maps => maps[0])
+			.then(gameService.getMap)
+			.then(map => {
+				ctrl.cities = map.Cities;
+				console.log(map);
+
+			});*/
+
+		gameService.getCities()
+			.then(cities => {
+				ctrl.cities = cities;
+				console.log(cities);
+			});
+
+
+		gameService.getRegions()
+			.then(regions => {
+				ctrl.regions = regions;
+				console.log(regions);
+			});
+
+	};
 };
 
-angular.module('auth')
-    .component('game', {
-        bindings: {},
-        templateUrl: 'app/game/game.html',
-        controller: componentController
-    });
+angular.module('app')
+	.component('game', {
+		bindings: {},
+		templateUrl: 'app/game/game.html',
+		controller: componentController
+	});
