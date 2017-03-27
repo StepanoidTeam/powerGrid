b9d880c1264889f5c733f8218c3d2de5268bf942
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-03-28 02:05:06 +0300

    dirty fix for temporary server bug with empty chat socket response

--- App/api/api-ws-endpoints.js
+++ App/api/api-ws-endpoints.js
@@ -1,43 +1,43 @@
 'use strict';
 
 angular.module('app')
-    .service('apiWsEndpoints', function ($q, $http, tokenService, apiConfig) {
-        const svc = this;
-        //init
-        const webSocket = new WebSocket(apiConfig.wsUrl);
-        svc.wsSource = Rx.Observable
-            .fromEvent(webSocket, 'message')//'open' 'close' 'error'
-            .map(wsEvent => {
-                const dataRaw = wsEvent.data;
-                return JSON.parse(dataRaw);
-            });
+	.service('apiWsEndpoints', function ($q, $http, tokenService, apiConfig) {
+		const svc = this;
+		//init
+		const webSocket = new WebSocket(apiConfig.wsUrl);
+		svc.wsSource = Rx.Observable
+			.fromEvent(webSocket, 'message')//'open' 'close' 'error'
+			.map(wsEvent => {
+				const dataRaw = wsEvent.data;
+				return JSON.parse(dataRaw || "{}");//todo: dirty hack - remove
+			});
 
 
-        svc.wsSource.subscribe((data) => {
-            //console.log('wss',data);
-        });
+		svc.wsSource.subscribe((data) => {
+			//console.log('wss',data);
+		});
 
 
-        function wsRequest(wsMethod, wsData) {
+		function wsRequest(wsMethod, wsData) {
 
-            wsData.AuthToken = tokenService.getToken();
-            wsData.Type = wsMethod;
+			wsData.AuthToken = tokenService.getToken();
+			wsData.Type = wsMethod;
 
-            const requestString = JSON.stringify(wsData);
-            webSocket.send(requestString);
-        }
+			const requestString = JSON.stringify(wsData);
+			webSocket.send(requestString);
+		}
 
-        /* CHAT */
+		/* CHAT */
 
-        svc.chatSendMessage = function (message, subscriberId) {
-            //todo: remove inroom
-            //todo: rename toUserId to receiver or channelId/subscriberID
-            const wsData = {
-                Message: message,
-                To: subscriberId || null,
-                InRoomChannel: false
-            };
+		svc.chatSendMessage = function (message, subscriberId) {
+			//todo: remove inroom
+			//todo: rename toUserId to receiver or channelId/subscriberID
+			const wsData = {
+				Message: message,
+				To: subscriberId || null,
+				InRoomChannel: false
+			};
 
-            return wsRequest('CHAT', wsData);
-        };
-    });
+			return wsRequest('CHAT', wsData);
+		};
+	});
