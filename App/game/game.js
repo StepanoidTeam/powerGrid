87fcbb37f7462ae125ceeea5144f350219cbe95f
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-04-06 02:01:52 +0300

    connectors basic rendering

--- App/game/game.js
+++ App/game/game.js
@@ -7,17 +7,18 @@ var componentController = function ($location, gameService) {
 	ctrl.$onInit = function () {
 
 		/*gameService.getMaps()
-			.then(maps => maps[0])
-			.then(gameService.getMap)
-			.then(map => {
-				ctrl.cities = map.Cities;
-				console.log(map);
+		 .then(maps => maps[0])
+		 .then(gameService.getMap)
+		 .then(map => {
+		 ctrl.cities = map.Cities;
+		 console.log(map);
 
-			});*/
+		 });*/
 
 		gameService.getCities()
 			.then(cities => {
 				ctrl.cities = cities;
+				console.log(cities);
 			});
 
 
@@ -28,12 +29,17 @@ var componentController = function ($location, gameService) {
 			});
 
 		gameService.getConnectors()
-			.then(connectors=> {
+			.then(connectors => {
 				ctrl.connectors = connectors;
 				console.log(connectors);
 			})
-
 	};
+
+
+	ctrl.getCityCoords = function (cityId) {
+		return ctrl.cities && ctrl.cities.find(x => x.Id === cityId);
+	}
+
 };
 
 angular.module('app')
