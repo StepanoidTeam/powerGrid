c2252b184e48485aee85a3894191c55dc8be4e4e
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-05-31 00:59:46 +0300

    todos added

--- App/misc/init/init.js
+++ App/misc/init/init.js
@@ -1,20 +1,33 @@
 'use strict';
 
 var componentController = function ($location, versionService) {
-    const ctrl = this;
+	const ctrl = this;
 
-    ctrl.$onInit = function () {
-        versionService.getApiVersion().then(function () {
-            $location.path('/login');
-        }, function () {
-            $location.path('/error');
-        });
-    };
+	ctrl.$onInit = function () {
+		versionService.getApiVersion().then(function () {
+
+
+
+			//todo: check plyaer:
+			// is logged - to rooms
+			// is in room - to room
+			// is in game - to game
+
+			// else - login
+
+			$location.path('/login');
+
+
+
+		}, function () {
+			$location.path('/error');
+		});
+	};
 };
 
 angular.module('auth')
-    .component('init', {
-        bindings: {},
-        templateUrl: 'app/misc/init/init.html',
-        controller: componentController
-    });
+	.component('init', {
+		bindings: {},
+		templateUrl: 'app/misc/init/init.html',
+		controller: componentController
+	});
