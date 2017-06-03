df1ed0d6ea2001c63c9c9508c249d5148ed24956
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-06-04 02:03:20 +0300

    redundant login redirects removed

--- App/auth/current-user/current-user.js
+++ App/auth/current-user/current-user.js
@@ -1,34 +1,28 @@
 'use strict';
 
 var componentController = function ($location, authService) {
-    const ctrl = this;
+	const ctrl = this;
 
-    let isAuth = false;
-    ctrl.isAuthorized = function () {
-        return isAuth;
-    };
+	let isAuth = false;
+	ctrl.isAuthorized = () => isAuth;
 
-    ctrl.logout = function () {
-        authService.logout().then(() => {
-            $location.path('/login');
-        })
-    };
+	ctrl.logout = () => authService.logout();
 
-    ctrl.$onInit = function () {
-        authService.player.subscribe(player => {
-            if (player) {
-                ctrl.userName = player.Name;
-                isAuth = true;
-            } else {
-                isAuth = false;
-            }
-        });
-    };
+	ctrl.$onInit = function () {
+		authService.player.subscribe(player => {
+			if (player) {
+				ctrl.userName = player.Name;
+				isAuth = true;
+			} else {
+				isAuth = false;
+			}
+		});
+	};
 };
 
 angular.module('auth')
-    .component('currentUser', {
-        bindings: {},
-        templateUrl: 'app/auth/current-user/current-user.html',
-        controller: componentController
-    });
+	.component('currentUser', {
+		bindings: {},
+		templateUrl: 'app/auth/current-user/current-user.html',
+		controller: componentController
+	});
