f23ddfe65b33e06b5990cb2cb207dfecf676de77
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-06-04 00:56:05 +0300

    version svc redirect to error page on error

--- App/misc/version-service.js
+++ App/misc/version-service.js
@@ -1,9 +1,13 @@
 angular.module('app')
-.service('versionService', function ($location, apiEndpoints) {
-	const svc = this;
+	.service('versionService', function ($location, apiEndpoints) {
+		const svc = this;
 
-	svc.getApiVersion = function () {
-		return apiEndpoints.getVersion();
-	};
+		svc.getApiVersion = function () {
+			return apiEndpoints.getVersion().catch((error) => {
+				console.error(error);
+				$location.path('/error', error);
+				throw error;
+			});
+		};
 
-});
+	});
