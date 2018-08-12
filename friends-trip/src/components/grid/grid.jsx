e8b0f51e531274e44cb249aa86e141e539d7acee
Volodymyr Myshko <volodymyrm@wix.com>
2018-08-12 18:27:29 +0300

    date parsed to local with bugs + col reordering

--- friends-trip/src/components/grid/grid.jsx
+++ friends-trip/src/components/grid/grid.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import moment from "moment";
 
 import "./grid.less";
 
@@ -7,10 +8,8 @@ export default class Grid extends React.Component {
   // pageIndex = 1;
   // pageButtonCount = 10;
 
-  defaultRenderer = (item, fieldName) => item[fieldName];
-
-  oweRenderer = (item, fieldName) => {
-    return item[fieldName].map((owe, key) => {
+  oweRenderer = value => {
+    return value.map((owe, key) => {
       return (
         <div key={key} className={"pd-b-20"}>
           {owe.user} : {owe.amount}
@@ -19,19 +18,34 @@ export default class Grid extends React.Component {
     });
   };
 
+  dateRenderer = value => {
+    var localTime = moment
+      .utc(value)
+      .local()
+      .format("ddd DD MMM-YY HH:mm");
+
+    return localTime;
+  };
+
   fields = [
-    { name: "id", type: "text", width: 150, visible: false, title: "Id" },
+    {
+      name: "time",
+      type: "text",
+      width: 80,
+      title: "Time",
+      renderer: this.dateRenderer
+    },
+    { name: "description", type: "text", width: 100, title: "Description" },
     { name: "payer", type: "text", width: 100, title: "Payer" },
     { name: "fullAmount", type: "number", width: 80, title: "Amount" },
-    { name: "time", type: "text", width: 100, title: "Time" },
+
     {
       name: "owe",
       type: "text",
       width: 150,
       title: "Owe",
       renderer: this.oweRenderer
-    },
-    { name: "description", type: "text", width: 100, title: "Description" }
+    }
   ];
 
   render() {
@@ -41,7 +55,7 @@ export default class Grid extends React.Component {
       <table className={["grid", className].join(" ")}>
         <thead>
           <tr>
-            {this.fields.filter(f => f.visible !== false).map((f, key) => (
+            {this.fields.map((f, key) => (
               <td key={key} style={{ width: `${f.width}px` }}>
                 {f.title}
               </td>
@@ -53,7 +67,7 @@ export default class Grid extends React.Component {
             <tr key={key} onClick={() => onItemSelected(item)}>
               {this.fields.filter(f => f.visible !== false).map((f, key2) => (
                 <td key={key2}>
-                  {(f.renderer || this.defaultRenderer)(item, f.name)}
+                  {f.renderer ? f.renderer(item[f.name]) : item[f.name]}
                 </td>
               ))}
             </tr>
