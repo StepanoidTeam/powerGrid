32a4ad60a2e3fc85e0f0a859794a833dda0499d1
Volodymyr Myshko <volodymyrm@wix.com>
2018-08-15 03:38:33 +0300

    build

---
+++ friends-trip/dist/precache-manifest.dde115a2377d06268301cdcdcf23e63a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5530a0047b55c64a2244",
+    "url": "web.js"
+  },
+  {
+    "revision": "ab86047e574a2dda0a2e",
+    "url": "vendors.js"
+  },
+  {
+    "revision": "52f41f63158f6a696f8e",
+    "url": "runtime.js"
+  },
+  {
+    "revision": "dd9c5d915c3ad4b396b7",
+    "url": "login.js"
+  },
+  {
+    "revision": "25ca2ee8fce4a82d095e",
+    "url": "index.js"
+  },
+  {
+    "revision": "ab505166fa21d36c0aa21658af3d03ad",
+    "url": "index.html"
+  },
+  {
+    "revision": "cde969890a125b493777",
+    "url": "app.js"
+  }
+];
