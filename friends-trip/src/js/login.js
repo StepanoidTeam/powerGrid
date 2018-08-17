56c86ca9ea7525b69da826c075984da0d89ed7ad
Volodymyr Myshko <volodymyrm@wix.com>
2018-08-18 02:17:09 +0300

    login restyling

--- friends-trip/src/js/login.js
+++ friends-trip/src/js/login.js
@@ -1,18 +1,21 @@
 import React from "react";
 
 import app from "./app.js";
-import Dialog from "../components/dialog/dialog.jsx";
 import Overlay from "../components/overlay/overlay.jsx";
 
-import { SimpleDialog } from "rmwc/Dialog";
+import { Typography } from "rmwc/Typography";
+import { Button, ButtonIcon } from "rmwc/Button";
 import { Snackbar } from "rmwc/Snackbar";
 import { TextField } from "rmwc/TextField";
+import { LinearProgress } from "rmwc/LinearProgress";
+import { Elevation } from "rmwc/Elevation";
 
 import "../styles/app.less";
 
 export default class Login extends React.Component {
   state = {
     isLoading: false,
+    isError: false,
     errorModel: false,
     user: { username: "", password: "" }
   };
@@ -39,10 +42,14 @@ export default class Login extends React.Component {
 
   onError = errorModel => {
     this.setState({
-      errorModel
+      errorModel,
+      isError: true
     });
 
-    //setTimeout(() => this.setState({ errorModel: false }), 4000);
+    setTimeout(
+      () => this.setState({ errorModel: false, isError: false }),
+      1500
+    );
   };
 
   onUsernameChange(username) {
@@ -64,51 +71,50 @@ export default class Login extends React.Component {
   }
 
   render() {
-    const { errorModel = false, isLoading } = this.state;
+    const { errorModel, isError, isLoading } = this.state;
+
+    const errorMessage = (errorModel && errorModel.message) || "kakoy-to bag";
 
     return (
       <div>
-        <SimpleDialog
-          title="Error"
-          body={this.state.error}
-          open={true}
-          onClose={() => onClose()}
-          //onAccept={() => onClose(this.state.item)}
-          // onCancel={() => onClose()}
-        >
-          <TextField
-            outlined
-            label="Username"
-            value={this.state.user.username}
-            onChange={event => this.onUsernameChange(event.target.value)}
-          />
+        <Overlay>
+          <Elevation z="2" className="fl-col">
+            <div className="fl-col controls">
+              <Typography use="headline5" className="login-splash">
+                ✈️ Friends Trip
+              </Typography>
+              <TextField
+                outlined
+                label="Username"
+                value={this.state.user.username}
+                onChange={event => this.onUsernameChange(event.target.value)}
+              />
 
-          <TextField
-            outlined
-            label="Password"
-            type="password"
-            value={this.state.user.password}
-            onChange={event => this.onPasswordChange(event.target.value)}
-          />
+              <TextField
+                outlined
+                label="Password"
+                type="password"
+                value={this.state.user.password}
+                onChange={event => this.onPasswordChange(event.target.value)}
+              />
+            </div>
 
-          <div className="fl-row controls">
-            <button
-              className="btn-register"
-              type="button"
-              onClick={this.register}
-            >
-              *️⃣ Register
-            </button>
-            <button className="btn-login" type="submit" onClick={this.signin}>
-              ✅ Login
-            </button>
-          </div>
-        </SimpleDialog>
-
-        <Overlay isOpen={isLoading}>⏳Loading...</Overlay>
+            <div className="fl-row controls">
+              <Button type="button" onClick={this.register}>
+                <ButtonIcon use="fiber_new" />
+                Register
+              </Button>
+              <Button raised type="submit" onClick={this.signin}>
+                <ButtonIcon use="fingerprint" />
+                Login
+              </Button>
+            </div>
+            <LinearProgress determinate={!isLoading} />
+          </Elevation>
+        </Overlay>
 
         <Snackbar
-          show={errorModel}
+          show={this.state.isError}
           message={<div>⛔{errorModel.message || "kakoy-to bag"}</div>}
           alignStart
         />
