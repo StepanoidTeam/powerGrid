b7545d1bee429d11ab0a80c9a8dc56f88fbe7894
Volodymyr Myshko <volodymyrm@wix.com>
2018-08-26 20:45:05 +0300

    fakereport made empty

--- friends-trip/src/js/fakeReport.js
+++ friends-trip/src/js/fakeReport.js
@@ -1,8 +1,4 @@
 export const fakeReport = {
-  user: "Dawa",
-  balances: [
-    { user: "Igor", paidForYou: 2, oweToYou: 3, owe: 1 },
-    { user: "Bulko", paidForYou: 0, oweToYou: 0, owe: 0 },
-    { user: "Bob", paidForYou: 461.99, oweToYou: 0, owe: -461.99 }
-  ]
+  user: null,
+  balances: []
 };
