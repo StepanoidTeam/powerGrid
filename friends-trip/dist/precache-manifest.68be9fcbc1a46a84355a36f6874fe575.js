b75b4def21ac4733b840364df741cdc26b6d38f1
Volodymyr Myshko <volodymyrm@wix.com>
2018-09-21 16:55:35 +0300

    build?

---
+++ friends-trip/dist/precache-manifest.68be9fcbc1a46a84355a36f6874fe575.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "08ea41a18f1b14e12224",
+    "url": "web.js"
+  },
+  {
+    "revision": "a16f8828abd01e7dc74e",
+    "url": "vendors.js"
+  },
+  {
+    "revision": "52f41f63158f6a696f8e",
+    "url": "runtime.js"
+  },
+  {
+    "revision": "f1f5c54c317e6bc98bb9",
+    "url": "login.js"
+  },
+  {
+    "revision": "37b171c877438713acb8",
+    "url": "index.js"
+  },
+  {
+    "revision": "a81e5a16cea0b1a961918c69e90e3020",
+    "url": "index.html"
+  },
+  {
+    "url": "e79bfd88537def476913f3ed52f4f4b3.eot"
+  },
+  {
+    "revision": "5ee9e6ff23b6ed316289",
+    "url": "app.js"
+  },
+  {
+    "url": "a37b0c01c0baf1888ca812cc0508f6e2.ttf"
+  },
+  {
+    "url": "570eb83859dc23dd0eec423a49e147fe.woff2"
+  },
+  {
+    "url": "012cf6a10129e2275d79d6adac7f3b02.woff"
+  }
+];
